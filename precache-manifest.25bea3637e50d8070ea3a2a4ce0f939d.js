c4dece9486c3dcb854864f3b2bee2b0daddf8157
THINK\Alexu <undynko@yahoo.de>
2019-02-18 17:18:03 +0100

    Updates

---
+++ precache-manifest.25bea3637e50d8070ea3a2a4ce0f939d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "ff0033b4ef0e6e0216ec",
+    "url": "/react-app/static/js/main.ff0033b4.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/react-app/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "ff0033b4ef0e6e0216ec",
+    "url": "/react-app/static/css/main.89e87672.chunk.css"
+  },
+  {
+    "revision": "90421f27e6414f2dc0ffe9ea232f6146",
+    "url": "/react-app/index.html"
+  }
+];
